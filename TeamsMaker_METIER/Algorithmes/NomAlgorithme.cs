c9a8f8e5f7bb6126f2ed10ca98d3bc3dd7d91223
RomainSouveton <romain.souveton@etu.u-bourgogne.fr>
2025-06-01 18:27:40 +0200

    Revert "suppression de l'algo useless de la base"
    This reverts commit f5f28811f6241bc3e61876b8df3dab314aa9f6de.

---
+++ TeamsMaker_METIER/Algorithmes/NomAlgorithme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsMaker_METIER.Algorithmes
+{
+    /// <summary>
+    /// Liste des noms d'algorithmes
+    /// </summary>
+    public enum NomAlgorithme
+    {
+        ALGOTEST,
+        GLOUTONCOIRSSANT
+    }
+
+
+    public static class NomAlgorithmeExt
+    {
+        /// <summary>
+        /// Affichage du nom de l'algorithme
+        /// </summary>
+        /// <param name="algo">NomAlgorithme</param>
+        /// <returns>La chaine de caractères à afficher</returns>
+        public static string Affichage(this NomAlgorithme algo)
+        {
+            string res = "Algorithme non nommé :(";
+            switch(algo)
+            {
+                case NomAlgorithme.ALGOTEST: res = "Algorithme de test (à supprimer)"; break;
+                case NomAlgorithme.GLOUTONCOIRSSANT: res = "Algo glouton"; break;
+            }
+            return res;
+        }
+    }
+}
